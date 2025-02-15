ed3ca5c6cc1aef655730eeda5441333786472342
Simon Morgan <sjm@sjm.io>
2025-02-15 17:21:11 +0000

    Add SOS sketch

---
+++ SOS/SOS.ino
@@ -0,0 +1,53 @@
+int OUTPUT_PIN = 13;
+
+int UNIT_OF_TIME = 500; // 1 second
+
+int DOT_TIME = UNIT_OF_TIME;
+int DASH_TIME = 3 * UNIT_OF_TIME;
+int DELAY_TIME = 3 * UNIT_OF_TIME;
+
+void setup() {
+  pinMode(OUTPUT_PIN, OUTPUT);
+}
+
+void loop() {
+  dot();
+  dot();
+  dot();
+
+  pause();
+
+  dash();
+  dash();
+  dash();
+
+  pause();
+
+  dot();
+  dot();
+  dot();
+
+  pause();
+  pause();
+}
+
+void dot() {
+  digitalWrite(OUTPUT_PIN, HIGH);
+  delay(DOT_TIME);
+
+  digitalWrite(OUTPUT_PIN, LOW);
+  delay(DELAY_TIME);
+}
+
+void dash() {
+  digitalWrite(OUTPUT_PIN, HIGH);
+  delay(DASH_TIME);
+
+  digitalWrite(OUTPUT_PIN, LOW);
+  delay(DELAY_TIME);
+}
+
+void pause() {
+  digitalWrite(OUTPUT_PIN, LOW);
+  delay(DELAY_TIME);
+}
