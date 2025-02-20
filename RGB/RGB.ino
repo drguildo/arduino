351d866df3d8fdefd775c2cf6d4805978d3ea334
Simon Morgan <sjm@sjm.io>
2025-02-20 19:39:09 +0000

    Add a fourth LED and rearrange pins

--- RGB/RGB.ino
+++ RGB/RGB.ino
@@ -1,13 +1,15 @@
-int LED_PIN_RED = 11;
+int LED_PIN_YELLOW = 10;
+int LED_PIN_BLUE = 11;
 int LED_PIN_GREEN = 12;
-int LED_PIN_BLUE = 13;
+int LED_PIN_RED = 13;
 
 void setup() {
   randomSeed(2381);
 
-  pinMode(LED_PIN_RED, OUTPUT);
-  pinMode(LED_PIN_GREEN, OUTPUT);
+  pinMode(LED_PIN_YELLOW, OUTPUT);
   pinMode(LED_PIN_BLUE, OUTPUT);
+  pinMode(LED_PIN_GREEN, OUTPUT);
+  pinMode(LED_PIN_RED, OUTPUT);
 }
 
 void loop() {
@@ -18,25 +20,30 @@ void loop() {
 }
 
 void enableRandomLights() {
-  long randomPinSelection = random(0, 8);
+  long randomPinSelection = random(0, 16);
 
   reset();
 
   if (bitRead(randomPinSelection, 0) == 1) {
-    digitalWrite(LED_PIN_RED, HIGH);
+    digitalWrite(LED_PIN_YELLOW, HIGH);
   }
 
   if (bitRead(randomPinSelection, 1) == 1) {
-    digitalWrite(LED_PIN_GREEN, HIGH);
+    digitalWrite(LED_PIN_BLUE, HIGH);
   }
 
   if (bitRead(randomPinSelection, 2) == 1) {
-    digitalWrite(LED_PIN_BLUE, HIGH);
+    digitalWrite(LED_PIN_GREEN, HIGH);
+  }
+
+  if (bitRead(randomPinSelection, 3) == 1) {
+    digitalWrite(LED_PIN_RED, HIGH);
   }
 }
 
 void reset() {
-  digitalWrite(LED_PIN_RED, LOW);
-  digitalWrite(LED_PIN_GREEN, LOW);
+  digitalWrite(LED_PIN_YELLOW, LOW);
   digitalWrite(LED_PIN_BLUE, LOW);
+  digitalWrite(LED_PIN_GREEN, LOW);
+  digitalWrite(LED_PIN_RED, LOW);
 }
